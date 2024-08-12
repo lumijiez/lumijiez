3acbc05a054a76c890e4931db13854a8ca2f3055
Daniel <danthevip@gmail.com>
2024-08-12 23:40:33 +0300

    updated logic

--- scripts/update.js
+++ scripts/update.js
@@ -1,25 +1,36 @@
 const fs = require('fs');
 const path = require('path');
+const axios = require('axios');
 
-const now = new Date();
-const utcTime = now.getTime() + now.getTimezoneOffset() * 60000;
+async function fetchWeather() {
+    const url = 'https://api.open-meteo.com/v1/forecast';
+    const params = {
+        latitude: 47.0105,
+        longitude: 28.8638,
+        hourly: 'temperature_2m'
+    };
 
-const gmtPlus3Time = new Date(utcTime + 3 * 3600000);
-const time = gmtPlus3Time.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });
-
-const badgeContent = `![Time](https://img.shields.io/badge/Time-${encodeURIComponent(time)}-blue)`;
-
-const readmePath = path.join(__dirname, '../README.md');
-let readmeContent = fs.readFileSync(readmePath, 'utf8');
-
-const timeBadgeRegex = /!\[Time\]\(https:\/\/img.shields.io\/badge\/Time-[^\)]+\)/;
-if (timeBadgeRegex.test(readmeContent)) {
-  readmeContent = readmeContent.replace(timeBadgeRegex, badgeContent);
-} else {
-  const badgeInsertionPoint = '## 🕒 Current Time\n\n';
-  readmeContent = readmeContent.replace(badgeInsertionPoint, `${badgeInsertionPoint}${badgeContent}\n`);
+    try {
+        const response = await axios.get(url, { params });
+        const currentTemperature = response.data.hourly.temperature_2m[0];  
+        return currentTemperature;
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        return 'unknown';
+    }
 }
 
-fs.writeFileSync(readmePath, readmeContent);
 
-console.log('README.md updated with time badge:', time);
+async function updateReadme() {
+    const time = new Date().toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });
+    const temperature = await fetchWeather();
+    const badgeContent = `![Time](https://img.shields.io/badge/Time-${encodeURIComponent(time)}-blue) ![Temperature](https://img.shields.io/badge/Temperature-${temperature}°C-red)`;
+    const readmePath = path.join(__dirname, '../README.md');
+
+    const readmeContent = fs.readFileSync(readmePath, 'utf8');
+    const updatedReadmeContent = readmeContent.replace(/!\[Time\]\(.*?\)/, badgeContent);
+
+    fs.writeFileSync(readmePath, updatedReadmeContent);
+}
+
+updateReadme();
