f7191f5b47bbe80bb87aca66e21d4c7c206dae7d
Daniel <59575049+lumijiez@users.noreply.github.com>
2024-08-12 23:03:10 +0300

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,1 +1,10 @@
+const fs = require('fs');
 
+function updateTimeBadge() {
+    const currentTime = new Date().toLocaleString();
+    const badgeContent = `![Time](https://img.shields.io/badge/Time-${encodeURIComponent(currentTime)}-blue)`;
+
+    fs.writeFileSync('time_badge.md', badgeContent);
+}
+
+updateTimeBadge();
