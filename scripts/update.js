e1231156eb81f31f844e3ce691ac024bfeeffc3b
Daniel <59575049+lumijiez@users.noreply.github.com>
2024-08-12 23:11:54 +0300

    Update update.js

--- scripts/update.js
+++ scripts/update.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 
-function updateTimeBadge() {
-    const currentTime = new Date().toLocaleString();
-    const badgeContent = `![Time](https://img.shields.io/badge/Time-${encodeURIComponent(currentTime)}-blue)`;
+const now = new Date();
+const utcTime = now.getTime() + now.getTimezoneOffset() * 60000;
 
-    fs.writeFileSync('time_badge.md', badgeContent);
-}
+const gmtPlus3Time = new Date(utcTime + 3 * 3600000);
+const time = gmtPlus3Time.toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });
 
-updateTimeBadge();
+const badgeContent = `![Time](https://img.shields.io/badge/Time-${encodeURIComponent(time)}-blue)`;
+
+const badgePath = path.join(__dirname, '../time_badge.md');
+fs.writeFileSync(badgePath, badgeContent);
+
+console.log('Badge updated with time:', time);
